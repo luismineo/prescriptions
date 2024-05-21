d6909ea7454bae74fc8f188ceb7ccd3abd61101c
Luis Mineo <luisafonsomineo@gmail.com>
2024-05-21 19:41:41 -0300

    initial commit

---
+++ src/main/java/com/rgls/medpresc/MedPrescApplication.java
@@ -0,0 +1,13 @@
+package com.rgls.medpresc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedPrescApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedPrescApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rgls/medpresc/MedPrescApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rgls.medpresc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedPrescApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
